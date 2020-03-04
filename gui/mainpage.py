1eba98abd12d6a0b85b52a4a43fa6743008a91ce
roy929 <royzelig@gmail.com>
2020-03-04 19:19:04 +0200

    now controls the calling; major changes

--- gui/mainpage.py
+++ gui/mainpage.py
@@ -1,48 +1,70 @@
 from tkinter import *
 from tkinter.ttk import *
-import sockets.user as user
+from sockets import user, voice
 from threading import Thread
 
 
-class MainPage(Thread):
+class MainPage:
     MY_USER_NAME = ""
     USER_TO_CALL = ""
+    stop = False
 
     def __init__(self, my_user_name, win=None):
-        super().__init__()
+        # Thread.__init__(self)
         self.MY_USER_NAME = my_user_name
-        if win is None:
+        self.win = win
+        if self.win is None:
             self.win = Tk()
-        else:
-            self.win = win
-
         self.win.title('User')
         self.style = Style(self.win)
         self.frame = Frame(self.win)
-        self.name = Label(self.frame, text='Call to').pack(side=TOP)
+        self.text1 = Label(self.frame, text='Call to')
+        self.text2 = Label(self.frame)
         self.user_to_call = Entry(self.frame)
-        self.call = Button(self.frame, text='Call', command=self.collect_viable_name)
-        self.end_call = Button()
+        self.call = Button(self.frame, text='Call', command=self.make_a_call)
+        self.end_call = Button(self.frame, text='End Call', command=self.stop_call)
+        self.end_chat = Button(self.frame, text='End Chat', command=voice.end)
 
-    def try_call(self):
+    def run(self):
         pop_up_message("calling '{0}'".format(self.USER_TO_CALL))
         self.call.forget()
         self.user_to_call.forget()
-        # self.win.withdraw()
-        self.start()  # threaded, the chat function
-        # find a way to know when chat ended
-        self.end_call = Button(self.frame, text='End Call', command=user.end_chat)
+        self.text1.forget()
+        self.text2.configure(text='Calling {0}...'.format(self.USER_TO_CALL))
+        self.text2.pack()
+        user_ip = user.get_user_ip(self.USER_TO_CALL)
         self.end_call.pack()
-        # self.win.update()
-        # self.win.deiconify()
+        # get ok from service
+        return_val = user.call_service(self.MY_USER_NAME, user_ip)
+        # find a way to break if waiting too long or if user wants to ?!!!!!
 
-    def collect_viable_name(self, event=None):
+        if return_val and not self.stop:
+            self.text2.configure(text='In chat with {0}'.format(self.USER_TO_CALL))
+            self.end_chat.pack()
+            # start chat
+            voice.start()
+        elif return_val == 'different':
+            pass
+        elif self.stop:
+            pop_up_message("call stopped")
+        else:
+            pop_up_message('sorry, an error occurred')
+        self.text2.forget()
+        self.end_call.forget()
+        self.starting_page()
+
+    def stop_call(self):
+        self.stop = True
+
+    def make_a_call(self, event=None):
         user_name = self.user_to_call.get()
         self.user_to_call.delete(0, len(user_name))
         if len(user_name) > 2 and user_name != self.MY_USER_NAME:
             if user.check_if_name_exists(user_name):  # check if the name database
                 self.USER_TO_CALL = user_name
-                self.try_call()
+                # start call
+                t = Thread(target=self.run)
+                t.start()
             else:
                 pop_up_message("sorry, the user '{0}' is not registered yet".format(user_name))
         elif len(user_name) < 3:
@@ -50,18 +72,16 @@ class MainPage(Thread):
         else:
             pop_up_message("sorry, you can't call yourself")
 
-    def to_run(self):
+    def starting_page(self):
         # packing and set up
+        self.text1.pack(side=TOP)
         self.user_to_call.pack()
         self.call.pack()
         self.frame.pack()
-        self.frame.bind_all('<Return>', self.collect_viable_name)
+        self.frame.bind_all('<Return>', self.make_a_call)
         self.user_to_call.focus_set()
         center_window(self.win)
         self.win.mainloop()
-
-    def run(self):
-        user.call_user(self.USER_TO_CALL, self.MY_USER_NAME)
 
 
 def pop_up_message(text):
@@ -87,10 +107,6 @@ def center_window(root, width=300, height=200):
 
 
 if __name__ == '__main__':
-    name = 'test'
-    conn = MainPage(name)
-    conn.to_run()
-    print('here')
-    while not True:
-        pass
-    # now chat
+    my_name = 'test'
+    conn = MainPage(my_name)
+    conn.starting_page()
